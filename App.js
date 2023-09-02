c3a03199c0fcfd04c9c96d1a00a2e0b3a32ec469
FATMA YILDIZ AKÇİÇEK <fatmayildiz@msn.com>
2023-09-02 09:31:10 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // CoinDesk API'den verileri almak için Axios kullanalım
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => {
+        setData(Object.entries(response.data.bpi));
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.header}>Kripto Para Fiyatları</Text>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item[0]}
+          renderItem={({ item }) => (
+            <View style={styles.currencyContainer}>
+              <Text style={styles.currency}>{item[0]}</Text>
+              <Text style={styles.price}>{item[1].rate} {item[1].description}</Text>
+            </View>
+          )}
+        />
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'darkblue',
+    padding: 20,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  header: {
+    fontSize: 24,
+    color: 'white',
+    marginBottom: 20,
+    marginTop:73,
+  },
+  currencyContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  currency: {
+    fontSize: 18,
+    color: 'white',
+  },
+  price: {
+    fontSize: 18,
+    color: 'white',
   },
 });
